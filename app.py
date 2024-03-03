7fa315fd108ba86b3b9f8f7f3bbc9c91b3a79646
Caitlin <caitlin.pett@gmail.com>
2024-03-03 11:30:08 +1000

    browse done

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route('/')                                                                 #use the route() decorator to tell Flask what URL should trigger our functions
-def index():                                                                    # createa a route for the landing / home page  
-    return render_template('index.html')    
+  
 
 if __name__ == '__main__':
     app.run()
+    
+@app.route('/')                                                                
+def index():                                                                    
+    return render_template('index.html')  
+                                                              
+
+@app.route('/browse')                                                                
+def browse():  
+    conn = get_db_connection()                                                  
+    properties = conn.execute('SELECT * FROM properties').fetchall()                
+    conn.close()                       
+    return render_template('browse.html', properties=properties)    
+
+
+
+
+def get_db_connection():
+    conn = sqlite3.connect('QPC.db')
+    conn.row_factory = sqlite3.Row
+    return conn
