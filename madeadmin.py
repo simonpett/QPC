db5eb47855957996a7053858937b09d5a13b1fee
Simon <simon.pett@gmail.com>
2024-04-20 18:03:03 +1000

    white space

--- madeadmin.py
+++ madeadmin.py
@@ -1,10 +1,10 @@
-import sqlite3  # make a user an admin, change email to the email of the user you want to make an admin
-conn = sqlite3.connect('QPC.db')    # Connect to the SQLite database
-cursor = conn.cursor()              # Create a cursor object and then execute the sql to make the user an admin
+import sqlite3  # make user an admin, change email to the value of the user you want to make admin
+conn = sqlite3.connect('QPC.db') # Connect to the SQLite database, then create the cursor
+cursor = conn.cursor()           # below - execute the sql to make the user an admin
 cursor.execute("UPDATE users SET is_admin = TRUE WHERE email = 'johnsmith@gmail.com'")               
-conn.commit()                       # Commit the changes and close the database connection
-cursor.close()                      # Close the cursor
-conn.close()                        # Close the database connection
+conn.commit()                    # Commit the changes and close the database connection
+cursor.close()                   # Close the cursor
+conn.close()                     # Close the database connection
 
 
 
